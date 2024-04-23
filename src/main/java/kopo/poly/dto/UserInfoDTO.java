b1fa90b3c3ef01a8c8711cf6f9c17fb634c0ebe0
Hyeopgeon Lee <soft1318@naver.com>
2024-04-23 13:41:51 +0900

    Spring Session 추가

--- src/main/java/kopo/poly/dto/UserInfoDTO.java
+++ src/main/java/kopo/poly/dto/UserInfoDTO.java
@@ -10,6 +10,8 @@ import kopo.poly.util.DateUtil;
 import kopo.poly.util.EncryptUtil;
 import lombok.Builder;
 
+import java.io.Serializable;
+
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserInfoDTO(
@@ -46,7 +48,7 @@ public record UserInfoDTO(
         // 회원가입시, 중복가입을 방지 위해 사용할 변수
         // DB를 조회해서 회원이 존재하면 Y값을 반환함
         // DB테이블에 존재하지 않는 가상의 컬럼(ALIAS)
-        String existsYn) {
+        String existsYn) implements Serializable {
 
     /**
      * 패스워드, 권한 등 회원 가입을 위한 정보 만들기
