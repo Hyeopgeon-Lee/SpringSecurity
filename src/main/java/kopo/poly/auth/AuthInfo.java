d7450b8589e54742fa561ee0ecff4ac4d788b9e6
Hyeopgeon Lee <soft1318@naver.com>
2025-08-20 09:33:59 +0900

    소스 수정

--- src/main/java/kopo/poly/auth/AuthInfo.java
+++ src/main/java/kopo/poly/auth/AuthInfo.java
@@ -17,7 +17,6 @@ import java.util.Set;
  * @param userInfoDTO 로그인된 사용자 정보 UserInfoRepository로부터 조회된 정보를 저장하기 위한 객체
  */
 @Slf4j
-@Getter
 public record AuthInfo(UserInfoDTO userInfoDTO) implements UserDetails {
 
     /**
